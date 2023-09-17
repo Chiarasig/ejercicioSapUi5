2f5127bcd1ee61c8c0721d8056a823ef686f1c6c
Chiarasig <chiarasignori37@gmail.com>
2023-09-17 23:43:00 +0000

    Controller, view, constants and i18n settings for correct table rendering

--- ejercicio_sap_ui5/webapp/controller/Main.controller.js
+++ ejercicio_sap_ui5/webapp/controller/Main.controller.js
@@ -1,37 +1,42 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/proy/ejerciciosapui5/util/Formatter",
+    "com/proy/ejerciciosapui5/util/Constants",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Formatter , Filter, FilterOperator ) {
+    function (Controller, Formatter, Constants, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("com.proy.ejerciciosapui5.controller.Main", {
             Formatter: Formatter,
             onInit: function () {
-                const sPath = jQuery.sap.getModulePath("com.proy.ejerciciosapui5") + "/localService/Products.json";
+                const sPath = jQuery.sap.getModulePath(Constants.model.module) + Constants.model.productsJSON;
                 console.log(sPath);
                 var oModel = new sap.ui.model.json.JSONModel();
                 oModel.loadData(sPath);
                 console.log(oModel);
-                this.getView().setModel(oModel, "mock");
-                const model = this.getView().getModel("mock")
+                this.getView().setModel(oModel, Constants.model.mockModel);
+                const model = this.getView().getModel(Constants.model.mockModel)
                 console.log(model)
+                var oResourceModel = this.getOwnerComponent().getModel(Constants.model.i18n);
+                oResourceModel.enhance({ bundleName: Constants.model.bundleName });
+                sap.ui.getCore().getConfiguration().setLanguage(Constants.model.languageEn);
             },
             onSearch: function(oEvent) {
                 var aFilter = [];
                 const sQuery = oEvent.getSource().getValue();
                 if (sQuery && sQuery.length > 0) {
-                    var oFilter = new Filter("Name", FilterOperator.Contains, sQuery);
+                    var oFilter = new Filter(Constants.model.propertyToFilter, FilterOperator.Contains, sQuery);
                     aFilter.push(oFilter);
                 }
-                var oTable = this.byId("idProductsTable"); // Asegúrate de que el ID sea el correcto
-                var oBindingItems = oTable.getBinding("items");
-                oBindingItems.filter(aFilter, "Application");
-            }            
+                var oTable = this.byId(Constants.model.idProductsTable);
+                var oBindingItems = oTable.getBinding(Constants.model.items);
+                oBindingItems.filter(aFilter, Constants.model.FilterApplication);
+            }                                
         });
     });
---
+++ ejercicio_sap_ui5/webapp/util/Constants.js
@@ -0,0 +1,17 @@
+sap.ui.define([], function (){
+    'use strict';
+    return{
+        model: {
+            i18n: "i18n",
+            mockModel: "mock",
+            module: "com.proy.ejerciciosapui5",
+            productsJSON: "/localService/Products.json",
+            idProductsTable: "idProductsTable",
+            items: "items",
+            FilterApplication: "FilterApplication",
+            propertyToFilter: "Name",
+            bundleName: "com.proy.ejerciciosapui5.i18n.i18n",
+            languageEn: "en"
+        }
+    };
+}, true)
